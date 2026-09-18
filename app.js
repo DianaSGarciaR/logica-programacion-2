43592a40cba9bdd9ddf8a0cbf5021d6162cb6838
Diana Garcia <dianasgr12@gmail.com>
2026-09-17 20:00:36 -0600

    Feat se libera interfaz para pedir al usario ingresar la cantidad que desea calcular y se asigna diseño a la vista

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-let celsius = prompt("Ingresa la temperatura en grados Celsius");
+//let celsius = prompt("Ingresa la temperatura en grados Celsius");
 /* Grados celsius a Kelvin */
-let kelvin = 273.15;
-let fahrenheit = 1.8;
+const kelvin = 273.15;
+const fahrenheit = 1.8;
 
-let converkelvin = celsius + kelvin;
-let converFahrenheit = (celsius * fahrenheit) + 32;
+//let converkelvin = celsius + kelvin;
+//let converFahrenheit = (celsius * fahrenheit) + 32;
 
-console.log(converkelvin);
-console.log(converFahrenheit);
+//console.log(converkelvin);
+//console.log(converFahrenheit);
+
+const formGradoCelsius = document.querySelector("#formGradoCelsius");
+const resultKelvin = document.querySelector("#resultKelvin");
+const resultFahrenheit = document.querySelector("#resultFahrenheit");
 
 
+/* Obtener los datos del formulario */
+formGradoCelsius.addEventListener("submit", (event => {
+    // Evita que la página se recargue al presionar un botón submit.
+    event.preventDefault();
+
+    let inputGradoCelsius = document.querySelector("#inputGradoCelsius");
+
+    let celsius = Number(inputGradoCelsius.value);
+    let converkelvin = celsius + kelvin;
+    let converFahrenheit = (celsius * fahrenheit) + 32;
+
+    resultKelvin.value = converkelvin;
+    resultFahrenheit.value = converFahrenheit;
+
+
+}));
