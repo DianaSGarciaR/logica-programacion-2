3e037f26ec561718119c68ac550d366291d4a9c5
Diana Garcia <dianasgr12@gmail.com>
2026-09-17 18:25:32 -0600

    Feat convertir grados celsius a kelvin y fahrenheit por medio de prompt, el resultado se muestra en consola

---
+++ app.js
@@ -0,0 +1,12 @@
+let celsius = prompt("Ingresa la temperatura en grados Celsius");
+/* Grados celsius a Kelvin */
+let kelvin = 273.15;
+let fahrenheit = 1.8;
+
+let converkelvin = celsius + kelvin;
+let converFahrenheit = (celsius * fahrenheit) + 32;
+
+console.log(converkelvin);
+console.log(converFahrenheit);
+
+
